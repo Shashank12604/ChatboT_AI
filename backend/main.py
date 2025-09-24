2cd9eaebed17a038c317410c469a80239ccfa6a5
Shashank12604 <shashankshukla12604@gmail.com>
2025-09-24 12:48:47 +0530

    update

--- backend/main.py
+++ backend/main.py
@@ -145,4 +145,4 @@ def search(req: SearchRequest) -> List[SourceDoc]:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8001, reload=True)
